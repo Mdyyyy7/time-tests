8709975a2f27d0a0c53c24c87f995773570f6878
comp0233 <ucapmox@ucl.ac.uk>
2024-10-28 17:55:34 +0800

    chang_test_one

--- test_times.py
+++ test_times.py
@@ -1,11 +1,22 @@
 from times import compute_overlap_time,time_range
 
 
+
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large, short)
-    expected = [("2010-01-12 10:30:00", "2010-01-12 10:37:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+    expected = [("2010-01-12 10:30:00", "2010-01-12 10:36:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
     assert result == expected
 
-test_given_input()
+
+
+
+def test_given_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:10:00", "2010-01-12 12:40:00")
+    result = compute_overlap_time(large, short)
+    expected = []
+    assert result == expected
+
+#test_given_input()

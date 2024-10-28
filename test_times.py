5dbe06da3a33facb6f5b3259a2533fbaa6c1fc7f
comp0233 <ucapmox@ucl.ac.uk>
2024-10-28 18:51:41 +0800

    Add backward check

--- test_times.py
+++ test_times.py
@@ -7,16 +7,39 @@ def test_given_input():
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
     result = compute_overlap_time(large, short)
     expected = [("2010-01-12 10:30:00", "2010-01-12 10:36:00"), ("2010-01-12 10:38:00", "2010-01-12 10:45:00")]
+
     assert result == expected
 
 
 
 
-def test_given_input():
+def test_given_nooverlap_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 12:10:00", "2010-01-12 12:40:00")
+    result = compute_overlap_time(large, short)
+ #   for start, end in result:
+  #      if(start>=end):
+   #         raise ValueError("start time cannot larger than end")
+    expected = []
+    assert result == expected
+
+def test_given_equal_input():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 12:40:00")
     result = compute_overlap_time(large, short)
     expected = []
     assert result == expected
 
+
+def test_given_large_input():
+    large = time_range("2010-01-12 13:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 12:40:00")
+    result = compute_overlap_time(large, short)
+    expected = []
+    assert result == expected
+
+
 #test_given_input()
+#test_given_nooverlap_input()
+#test_given_equal_input()
+test_given_large_input()
